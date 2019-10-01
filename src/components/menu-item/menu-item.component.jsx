c2252f1a902e09979b7ba287a89a8af1e04ab9ad
unknown <thiago8903@hotmail.com>
2019-09-30 21:42:41 -0300

    Commit inicial capitulo 1

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import './menu-item.scss'
+
+export default class MenuItem extends React.Component {
+
+    render() {
+        
+        const {title, imageUrl, size} = this.props
+
+        return (
+            <div className={`${size} menu-item`}>
+                <div className="background-image" 
+                    style={{
+                        backgroundImage: `url(${imageUrl})`                    
+                    }}
+                />
+                <div className='content'>
+                    <h1 className='title'>{title.toUpperCase()}</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+        )
+    }
+
+}
