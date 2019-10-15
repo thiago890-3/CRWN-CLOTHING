7ae4c9ce6850820d35340d35d9de82152d169dfa
thiago.oalmeida <thiago.oalmeida@rjod270502.prevnet>
2019-10-15 13:36:41 -0300

    Adição de redux para cart, configuração de redux em collection-item

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -13,7 +13,7 @@ const CollectionPreview = ({title, items}) => (
                 items
                 .filter((item, idx) => idx < 4)
                 .map((item) => (
-                    <CollectionItem key={item.id} {...item}/>                    
+                    <CollectionItem key={item.id} item={item}/>                    
                 ))
             }
             
