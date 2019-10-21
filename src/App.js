ce6538a14d3e9c37e664023d6c59dede3906cc65
thiago.oalmeida <thiago.oalmeida@rjod270502.prevnet>
2019-10-21 13:07:57 -0300

    Criação de collection-overview, criação de directory reducer, criação de shop reducer

--- src/App.js
+++ src/App.js
@@ -47,8 +47,6 @@ class App extends React.Component {
   }
 
   render() {
-    console.log(this.props.currentUser)
-
     return (
       <div>
         <Header />
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,37 +1,14 @@
 import React from 'react'
 
-import SHOP_DATA from './shop.data.js'
+import CollectionsOverview from '../../components/collection-overview/collection-overview.component'
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+const ShopPage = ({ collections }) => (
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            collections: SHOP_DATA
-        }
-
-    }
-
-    render() {
-        const { collections } = this.state
-
-        return (
-
-            <div className='shop-page'>
-                {
-                    collections
-                        
-                        .map(({ id, ...otherCollectionProps }) => {
-                            return <CollectionPreview key={id} {...otherCollectionProps} />
-                        })
-                }
-            </div>
-        )
+        <div className='shop-page'>
+            <CollectionsOverview />
+        </div>
+)
 
 
-    }
-}
 
 export default ShopPage
